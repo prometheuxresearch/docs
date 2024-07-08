f5394f3cf19c18340e29b3043ca3f98479705950
Joel Kang <joel@prometheux.co.uk>
2024-07-08 14:21:45 +0100

    fix(config): set baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   favicon: 'img/logo.svg',
 
   // Set the production url of your site here
-  url: 'https://prometheux.github.io',
+  url: 'prometheuxresearch.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

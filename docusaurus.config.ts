da0b28b1c882879545e9a2dc580660e26b8e1be3
Joel Kang <joel@prometheux.co.uk>
2024-08-17 07:43:31 +0100

    feat(docusaurus): add algolia search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,6 +58,39 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "DCCC0T0ITC",
+
+      // Public API key: it is safe to commit it
+      apiKey: "870d45e2eaf4483e87c2204607df57c7",
+
+      indexName: "prometheux-co",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push.
+      // Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/",
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
     navbar: {
       title: "",
       logo: {
